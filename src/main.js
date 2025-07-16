a2c7a5ce46142729b56c9f243f4eb9723d4cdd1e
Jorge <jorge.casanova.sanchez@gmail.com>
2025-07-16 11:29:43 +0200

    rutas arregladas al css

---
+++ src/main.js
@@ -0,0 +1,8 @@
+// main.jsx
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './styles/globals.css';  // Actualiza la ruta
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
